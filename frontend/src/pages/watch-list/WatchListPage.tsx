b13ac847518de2183e12e6f8154fb6bc7cddff5d
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-21 10:56:09 +0200

    create component card

--- frontend/src/pages/watch-list/WatchListPage.tsx
+++ frontend/src/pages/watch-list/WatchListPage.tsx
@@ -1,5 +1,17 @@
+import Card from "../../components/card/Card";
+
 const WatchListPage = () => {
-  return <div>WatchListPage</div>;
+  return (
+    <div>
+      <Card variant="collection">
+        <div>
+          <div>aaaa</div>
+          <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+          <div>aaaa</div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default WatchListPage;
