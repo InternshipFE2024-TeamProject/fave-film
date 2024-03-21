d3a6eecc0b7ad3b64a0268ee2df2463463c4803c
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-21 11:07:29 +0200

    Clean up code in watch list page

--- frontend/src/pages/watch-list/WatchListPage.tsx
+++ frontend/src/pages/watch-list/WatchListPage.tsx
@@ -1,17 +1,5 @@
-import Card from "../../components/card/Card";
-
 const WatchListPage = () => {
-  return (
-    <div>
-      <Card variant="collection">
-        <div>
-          <div>aaaa</div>
-          <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
-          <div>aaaa</div>
-        </div>
-      </Card>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default WatchListPage;
