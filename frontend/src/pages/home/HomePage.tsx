5c5965f8728719a1cefc66605ac12bf0e25d3f77
adrianacostep33 <adriana.coste@principal33.com>
2024-03-22 08:50:14 +0200

    Establish communication with backend

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,4 +1,32 @@
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query {
+    movieQuery {
+      movies {
+        id
+        title
+        description
+      }
+    }
+  }
+`;
+
+interface IMovie {
+  id: number;
+  title: string;
+  description: string;
+}
+
 const HomePage = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  console.log({ loading });
+  console.log({ error });
+  console.log({ data });
+
+  if (!data) return;
+  data.movieQuery.movies.map((movie: IMovie) => console.log(movie));
   return <div>HomePage</div>;
 };
 
